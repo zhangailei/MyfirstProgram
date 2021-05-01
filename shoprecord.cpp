178a72b6b9abd297e7b41d8db18640e96668eceb
DESKTOP-4RGMGPL\a1184 <lingxingshaonian@163.com>
2021-05-01 20:23:10 +0800

    shopfresh

--- shoprecord.cpp
+++ shoprecord.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "Sales_item.h"
+using namespace std;
 int main()
 {
-    std::cout << "Enter two numbers :" << std::endl;
+    Sales_item item, curitem;
+    if (cin >> item)
+    {
+
+        while (cin >> curitem)
+        {
+            if (item.isbn() == curitem.isbn())
+                item += curitem;
+            /* code */
+            else
+            {
+                cout << item << endl;
+                item = curitem;
+            }
+        }
+        cout << item << endl;
+    }
+    else
+    {
+        cerr << "no data?!!" << endl;
+        return -1;
+    }
     return 0;
 }
